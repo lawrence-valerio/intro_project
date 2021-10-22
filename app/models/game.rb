06e2cee5fa9dee060b284f99d127aa6bd705b74f
Lawrence Valerio <valerio.lawrence@gmail.com>
2021-10-22 12:52:11 -0500

    Created joiner table for Games and Publishers.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,7 @@
 class Game < ApplicationRecord
   has_many :game_genres
   has_many :genres, through: :game_genres
+
+  has_many :game_publishers
+  has_many :publishers, through: :game_publishers
 end
--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,2 +1,4 @@
 class Publisher < ApplicationRecord
+  has_many :game_publishers
+  has_many :games, through: :game_publishers
 end
